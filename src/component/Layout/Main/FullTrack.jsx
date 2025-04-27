f4fee36e5a493fd967c24ecbdfb15ca4eeebd2a5
Sofa34636 <www.sofalis21@gmail.com>
2025-04-27 13:09:02 +1000

    Добавление страницы для трека

--- src/component/Layout/Main/FullTrack.jsx
+++ src/component/Layout/Main/FullTrack.jsx
@@ -28,62 +28,67 @@ export function FullTrack() {
     <section className='FullTrack'>
       <div className='container'>
         <div className='fullTrack__inner'>
-          <div className=''>
-            <h2>{id}</h2>
+          <div class='fullTrack__titel'>
             <h2>Информация о треке</h2>
-            <TrackFull key={track?.id} {...track} />
-            <div className=''>
-              <p className=''>Исполнитель</p>
-              <p>{track.Artist}</p>
-            </div>
-            <div className=''>
-              <p className=''>Название</p>
-              <p>{track.Title}</p>
-            </div>
-            <div className=''>
-              <p className=''>Жанр</p>
-              <p>{track.Genre}</p>
-            </div>
-            <div className=''>
-              <p className=''>Темп</p>
-              <p>{track.Tempo}</p>
-            </div>
-            <div className=''>
-              <p className=''>Голос</p>
-              <p>{track.Voice}</p>
-            </div>
-            <div className=''>
-              <p className=''>Длительность</p>
-              <p>{track.Duration}</p>
-            </div>
-            <div className=''>
-              <p className=''>Язык</p>
-              <p>{track.Language}</p>
-            </div>
-            <div className=''>
-              <p className=''>Альбомы</p>
-              <p>{track.Albums}</p>
-            </div>
-          </div>
-          <div className=''>
             <h2>Информация об авторстве</h2>
-            <div className=''>
-              <p className=''>Композитор</p>
-              <p>{track.Composer}</p>
+          </div>
+          <div className='fullTrack__item'>
+            <div className='fullTrack__item-blok'>
+              <TrackFull key={track.id} {...track} />
+              <div className='fullTrack__flex fullTrack__color '>
+                <p className=''>Исполнитель</p>
+                <p>{track.Artist}</p>
+              </div>
+              <div className='fullTrack__flex fullTrack__color fullTrack__brown'>
+                <p className=''>Название</p>
+                <p>{track.Title}</p>
+              </div>
+              <div className='fullTrack__flex fullTrack__color '>
+                <p className=''>Жанр</p>
+                <p>{track.Genre}</p>
+              </div>
+              <div className='fullTrack__flex fullTrack__color fullTrack__brown'>
+                <p className=''>Темп</p>
+                <p>{track.Tempo}</p>
+              </div>
+              <div className='fullTrack__flex fullTrack__color '>
+                <p className=''>Голос</p>
+                <p>{track.Voice}</p>
+              </div>
+              <div className='fullTrack__flex fullTrack__color fullTrack__brown'>
+                <p className=''>Длительность</p>
+                <p>{track.Duration}</p>
+              </div>
+              <div className='fullTrack__flex fullTrack__color '>
+                <p className=''>Язык</p>
+                <p>{track.Language}</p>
+              </div>
+              <div className='fullTrack__flex fullTrack__color fullTrack__brown'>
+                <p className=''>Альбомы</p>
+                <p>{track.Albums}</p>
+              </div>
             </div>
-            <div className=''>
-              <p className=''>Текст</p>
-              <p>{track.Lyricist}</p>
-            </div>
-            <div className=''>
-              <p className=''>Аранжировка</p>
-              <p>{track.Arrangement}</p>
-            </div>
-            <div className=''>
-              <p className=''>Студия записи</p>
-              <p>{track.RecordingStudio}</p>
+            <div className='fullTrack__item-blok'>
+              <div className='fullTrack__flex fullTrack__color '>
+                <p className=''>Композитор</p>
+                <p>{track.Composer}</p>
+              </div>
+              <div className='fullTrack__flex fullTrack__color fullTrack__brown'>
+                <p className=''>Текст</p>
+                <p>{track.Lyricist}</p>
+              </div>
+              <div className='fullTrack__flex fullTrack__color '>
+                <p className=''>Аранжировка</p>
+                <p>{track.Arrangement}</p>
+              </div>
+              <div className='fullTrack__flex fullTrack__color fullTrack__brown'>
+                <p className=''>Студия записи</p>
+                <p>{track.RecordingStudio}</p>
+              </div>
             </div>
           </div>
+
+          <button className='Button Button__blue '>Отправить повторно</button>
         </div>
       </div>
     </section>
--- src/component/Layout/Main/TrackFull.jsx
+++ src/component/Layout/Main/TrackFull.jsx
@@ -3,11 +3,11 @@ import audio from '../../../assets/audio.mp3';
 import React from 'react';
 import Import from '../../../Images/icon/import.svg';
 
-export function TrackFull({ id, TrackNumber, Price, Duration }) {
+export function TrackFull({ TrackNumber, Price, Duration }) {
   return (
     <section>
       <div class='track'>
-        <div class='track__bloc cart__CartTracks'>
+        <div class='track__bloc trackFull__width'>
           <AudioPlayer audioFile={audio} />
           <div class='track__voice'>
             <div>{TrackNumber}</div>
